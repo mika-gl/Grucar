62edabf2a5a472b71cfa02f92e4158d42d8d81af
Mathews <mathewsalv@gmail.com>
2024-09-23 19:24:41 -0300

    se agrega perfil y direccion hacia

--- src/main/java/com/principal/grucar_proyecto/controllers/IndexController.java
+++ src/main/java/com/principal/grucar_proyecto/controllers/IndexController.java
@@ -3,9 +3,9 @@ package com.principal.grucar_proyecto.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.principal.grucar_proyecto.models.Cliente;
-import com.principal.grucar_proyecto.models.forms.Login;
+import com.principal.grucar_proyecto.models.Solicitud;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
@@ -17,5 +17,15 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index.jsp";
+    }
+
+    @GetMapping("/perfil")
+    public String perfilUsuario(HttpSession session, Model model, @ModelAttribute(name="solicitud") Solicitud solicitud) {
+        if (session.getAttribute("currentUser") == null) {
+            return "redirect:/";
+        }else
+        {
+            return "home/prestador/perfil.jsp";
+        }
     }
 }
